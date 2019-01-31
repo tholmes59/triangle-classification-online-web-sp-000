580735747a23f5bc8aa6408579009fbb3ac8bcd8
Tom Holmes <tholmes59@gmail.com>
2019-01-31 19:03:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,8 @@ class Triangle
       raise TriangleError
       elsif side1 < 0 || side2 < 0 || side3 < 0 
       raise TriangleError
+      elsif side1 += side2 < side3 || side1 += side3 < side2 || side2 += side3 < side1
+      raise TriangleError
     end
   end
   
