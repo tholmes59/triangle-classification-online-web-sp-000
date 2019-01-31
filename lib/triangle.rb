c4ad3f671bf9959d57b9c8d640d24a373590420c
Tom Holmes <tholmes59@gmail.com>
2019-01-31 18:45:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,11 @@
 class Triangle
   
-  attr_accessor :
+  attr_accessor :side1, :side2, :side3
   
-  def initialize()
-    
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end 
   
   def kind 
