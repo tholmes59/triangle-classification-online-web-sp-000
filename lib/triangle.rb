d40d8e3ce0e380b808ae285205ab9bee17d4ff29
Tom Holmes <tholmes59@gmail.com>
2019-01-31 19:19:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  def kind 
+     confirm_triangle
+      if side1 == side2 && side1 == side3
+        :equilateral
+      elsif side1 == side2 || side1 == side3 || side2 == side3
+        :isosceles
+      elsif side1 != side2 && side1 != side3 && side2 != side3
+        :scalene
+    end
+  end
+  
+  def confirm_triangle
+      if side1 == 0 && side2 == 0 && side3 == 0 
+      raise TriangleError
+      elsif side1 < 0 || side2 < 0 || side3 < 0 
+      raise TriangleError
+      elsif (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)
+      raise TriangleError
+    end
+  end
+  
+  class TriangleError < StandardError
+    "You are violating the laws of triangles!"
+  end
+  
 end
