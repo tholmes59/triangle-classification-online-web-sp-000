aa71a23b7f6d40b029f45f0ff627d57a7dade314
Tom Holmes <tholmes59@gmail.com>
2019-01-31 19:14:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   
   def kind 
      confirm_triangle
-      elsif side1 == side2 && side1 == side3
+      if side1 == side2 && side1 == side3
         :equilateral
       elsif side1 == side2 || side1 == side3 || side2 == side3
         :isosceles
@@ -19,15 +19,15 @@ class Triangle
     end
   end
   
-  # def confirm_triangle
-  #     if side1 == 0 && side2 == 0 && side3 == 0 
-  #     raise TriangleError
-  #     elsif side1 < 0 || side2 < 0 || side3 < 0 
-  #     raise TriangleError
-  #     elsif side1 += side2 < side3 || side1 += side3 < side2 || side2 += side3 < side1
-  #     raise TriangleError
-  #   end
-  # end
+  def confirm_triangle
+      if side1 == 0 && side2 == 0 && side3 == 0 
+      raise TriangleError
+      elsif side1 < 0 || side2 < 0 || side3 < 0 
+      raise TriangleError
+      elsif side1 += side2 < side3 || side1 += side3 < side2 || side2 += side3 < side1
+      raise TriangleError
+    end
+  end
   
   class TriangleError < StandardError
     # triangle error code
